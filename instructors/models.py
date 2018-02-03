10c2ba2199d3a8bb9c0c29d15fe3de7b367dc57d
Alina <xvxcherryxvx@gmail.com>
2018-02-03 21:00:05 +0200

    i i added some new objects to Instructors class and made migrations

--- instructors/models.py
+++ instructors/models.py
@@ -4,7 +4,30 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Instructors(models.Model): # має бути Instructorале в дб пишеться уже з буквою с
-    name = models.CharField(max_length = 64)
-    surname = models.CharField(max_length = 64)
-    cource = models.CharField(max_length = 64)
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Instructors(models.Model):
+    name = models.CharField(verbose_name=u'Instructor Name', max_length=255, help_text="This is name")
+    surname = models.CharField(max_length = 255)
+    date_of_birth = models.DateField(null=True, blank=True)
+    email = models.EmailField(unique=True, null=True)
+
+    course = models.ManyToManyField(Course)
+    is_active = models.BooleanField(default=True)
+
+    position = models.ForeignKey(Position, null=True)
+
+    def __str__(self):
+        return self.name
