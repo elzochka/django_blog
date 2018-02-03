7bf95036e1c7bcc283557610db1b2a5bf85f8c98
Alina <xvxcherryxvx@gmail.com>
2018-02-03 09:49:42 +0200

    registered in admin and added Instrustors to admin

--- instructors/admin.py
+++ instructors/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from instructors.models import Instructors
+
+admin.site.register(Instructors)
 
 # Register your models here.
